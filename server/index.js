7b1c322c06ff8d5baa516cf724f8a23c89ee99b3
Oshrat Tesema <oshratesema0@gmail.com>
2022-10-07 11:19:12 +0300

    edit

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+
+const app = express()
+require('dotenv').config();
+
+app.use(cors())
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser : true,
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log('DB Connection Successfully');
+}).catch((err) => {
+    console.log(err.message);
+})
+
+const server = app.listen(process.env.PORT, () => {
+    console.log(`server started on port ${process.env.PORT}`);
+})
